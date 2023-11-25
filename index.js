092008795d179fd6c4a74539cc3d4e4d4ff556ea
ronyahmed6245 <ronyahmed6245.gmail.com>
2023-11-26 01:06:26 +0600

    get the payments data

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,60 +9,64 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-// ---------- -----------
-// mongoDB code here
-// ----------------------
-// servemeal
-// x6aaqe7AkmgPL2GJ
-
+// MongoDB code
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.eehyjj4.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(process.env.MONGO_USER, process.env.MONGO_PASSWORD);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
 
 async function run() {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     // await client.connect();
-// operations starts ===>
+    console.log('Connected to MongoDB');
+
+    // Define routes after the MongoDB client is connected
+    const mealsCollection = client.db("mealsDB").collection("checkout");
+
+    app.get("/v1/paymentCard", async (req, res) => {
+      try {
+        const result = await mealsCollection.find().toArray();
+        console.log(result);
+        res.send(result);
+      } catch (error) {
+        console.error('Error in /v1/paymentCard route:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
 
 
 
 
 
+
+
+
+
+
+    
+    // Listen on the specified port
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
+    // console.log('Disconnected from MongoDB');
   }
 }
+app.get("/", (req, res) => {
+    res.send("Crud is running...");
+  });
+// Start the application
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
